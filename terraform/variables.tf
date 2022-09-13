dca91e4b256106f0accb1c2cdcd42ad5f9605eec
RTS-1989 <63114573+RTS-1989@users.noreply.github.com>
2022-09-13 17:31:24 +0500

    Changed .gitignore and varables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -27,3 +27,22 @@ variable "access_key" {
 variable "secret_key" {
   description = "secret"
 }
+variable "app_disk_image" {
+  description = "Disk image for reddit app"
+  default     = "reddit-base"
+}
+variable "db_disk_image" {
+  description = "Disk image for reddit db"
+}
+variable public_key_path {
+  description = "Path to the public key used for ssh access"
+}
+variable private_key_path {
+  description = "Path to the private key"
+}
+variable image_id {
+  description = "Disk image"
+}
+variable subnet_id{
+  description = "Subnet"
+}
